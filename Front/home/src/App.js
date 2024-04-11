d065700543d2465a9dd8e762241086edcc8aab4e
Dan1loJos <157768499+Dan1loJos@users.noreply.github.com>
2024-04-11 16:23:45 -0600

    Update App.js

--- Front/home/src/App.js
+++ Front/home/src/App.js
@@ -5,8 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+<<<<<<< Updated upstream
         <h1>Hola</h1>
         <h2>Estos son cambios</h2>
+=======
+>>>>>>> Stashed changes
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
